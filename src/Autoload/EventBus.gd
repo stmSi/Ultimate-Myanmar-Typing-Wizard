6787a5deb7f73d696d2d6a0296bb178e78d97409
stmSi <stm1998sithumyo@gmail.com>
2023-02-27 08:14:19 +0630

    Rework: Loading lesson and exercise strategy

--- src/Autoload/EventBus.gd
+++ src/Autoload/EventBus.gd
@@ -6,7 +6,7 @@ signal written_string_changed(str: String)
 signal select_lesson(lesson: String) # should be lesson file path
 signal select_next_lesson()
 
-signal assign_text(text: String)
+signal exercise_loaded(text: String)
 signal correct_text(char_pos: int)
 
 signal finished_section()
@@ -16,3 +16,5 @@ signal followup_popup_pos_changed(pos: Vector2)
 
 signal message_popup(msg: String)
 signal message_popup_closed()
+
+signal lesson_id_loaded(lesson_id: int)
--- src/Playground/Playground.gd
+++ src/Playground/Playground.gd
@@ -5,53 +5,76 @@ extends Control
 @onready var line_edit: LineEdit = %LineEdit
 @onready var status: Label = %Status
 
-var current_text = ''
+var current_exercise_text = ''
 
 var _eng_to_mm_converted = false
 
-
 var lesson_ids = []
+var lesson_idx = 0
+
 var exercises = []
+var exercise_idx = 0
+
+
+var difficulty = 'basic'
 
 func _ready():
 	EventBus.finished_section.connect(self._load_exercise)
 	EventBus.finished_all_sections.connect(self._finished_all_sections)
 
-	_start_exercise()
+	_start_lesson()
 
-func _start_exercise():
-	var files = LessonAccess.get_lesson_files('basic')
+func _start_lesson():
+	lesson_ids = []
+	exercises = []
+	lesson_idx = 0
+	exercise_idx = 0
+
+	var files: PackedStringArray = LessonAccess.get_lesson_files(difficulty)
+	if files.size() == 0:
+		EventBus.message_popup.emit(
+			"Error: difficulty '" \
+			+ difficulty.capitalize() \
+			+ "' is has no lessons. \r\n" + \
+			"Use Exercise Editor to create Lessons and Exercises."
+		)
+		return
 	files.sort()
 	for f in files:
 		lesson_ids.push_back(f.get_basename().get_file())
-	
-	exercises = LessonAccess.get_exercise_lines(int(lesson_ids[0]), 'basic')
-	lesson_ids.pop_front()
 	_load_exercise()
 
 func _load_exercise():
-	if exercises.size() == 0:
-		if lesson_ids.size() == 0:
+	if exercises.size() == exercise_idx:
+		if lesson_ids.size() == lesson_idx:
 			EventBus.finished_all_sections.emit()
 			return
 		else:
-			exercises = LessonAccess.get_exercise_lines(int(lesson_ids[0]), 'basic')
-			lesson_ids.pop_front()
-			if exercises.size() == 0:
-				_load_exercise() # keep loading lessons one by one until there is exercise
+			exercises = LessonAccess.get_exercise_lines(int(lesson_ids[lesson_idx]), difficulty)
+			EventBus.message_popup.emit("Lesson ID: " + lesson_ids[lesson_idx] + " is loaded.")
+			lesson_idx += 1
+
+			EventBus.lesson_id_loaded.emit(int(lesson_ids[lesson_idx]))
+			
+			
+			if exercises.size() == exercise_idx:
+				# keep loading lessons one by one until there is exercise 
+				# otherwise restart with `EventBus.finished_all_sections.emit()`
+				_load_exercise() 
 				return
 	
 	
-	current_text = exercises[0]
-	exercises.remove_at(0)
-	EventBus.assign_text.emit(current_text)
-	EventBus.current_char_changed.emit(current_text[0])
+	current_exercise_text = exercises[exercise_idx]
+	exercise_idx += 1
+	
+	EventBus.exercise_loaded.emit(current_exercise_text)
+	EventBus.current_char_changed.emit(current_exercise_text[0])
 
 
 func _finished_all_sections():
 #	$RestartDialog.show()
 	EventBus.message_popup.emit("All Lessons learned. Restarting...")
-	_start_exercise()
+	_start_lesson()
 
 
 func _on_text_edit_text_changed(_t: String) -> void:
@@ -63,18 +86,36 @@ func _on_text_edit_text_changed(_t: String) -> void:
 	_eng_to_mm_converted = false
 	
 	##### Status Update #####
-	if current_text.begins_with(line_edit.text):
+	if current_exercise_text.begins_with(line_edit.text):
 		status.text = "OK"
 	else:
 		status.text = "Error"
 
 	###### Update Next Char ######
 	var l = len(line_edit.text)
-	if l < len(current_text):
-		EventBus.current_char_changed.emit(current_text[l])
+	if l < len(current_exercise_text):
+		EventBus.current_char_changed.emit(current_exercise_text[l])
 	
 	EventBus.written_string_changed.emit(line_edit.text)
 	
 	###### Determine Finish Section #########
-	if current_text != '' and current_text == line_edit.text:
+	if current_exercise_text != '' and current_exercise_text == line_edit.text:
 		EventBus.finished_section.emit()	
+
+
+func _on_basic_btn_pressed() -> void:
+	difficulty = 'basic'
+	_start_lesson()
+	pass # Replace with function body.
+
+
+func _on_intermediate_btn_pressed() -> void:
+	difficulty = 'intermediate'
+	_start_lesson()	
+	pass # Replace with function body.
+
+
+func _on_advanced_btn_pressed() -> void:
+	difficulty = 'advanced'
+	_start_lesson()
+	pass # Replace with function body.
