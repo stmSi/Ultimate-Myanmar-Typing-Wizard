79e173b2dc25a70f294480c45f338367c9add4a0
stmSi <stm1998sithumyo@gmail.com>
2023-02-27 10:07:01 +0630

    Rework: Popup Message to allow multiple popup

--- src/MessagePopup/message_popup.gd
+++ src/MessagePopup/message_popup.gd
@@ -1,16 +1,18 @@
 extends VBoxContainer
+
 @onready var message_lbl: Label = %MessageLbl
 @onready var margin_container: MarginContainer = $MarginContainer
 @onready var ok_btn: Button = %OkBtn
 
-func _ready() -> void:
-	EventBus.message_popup.connect(self._on_message_popup)
-	
-func _on_message_popup(msg: String):
+
+func set_msg(msg: String) -> void:
 	message_lbl.text = msg
 	ok_btn.grab_focus()
 	$AnimationPlayer.play('open')
 
 func _on_ok_btn_pressed() -> void:
 	EventBus.message_popup_closed.emit()
-	$AnimationPlayer.play('close')
+	$AnimationPlayer.play('close') 
+	await $AnimationPlayer.animation_finished
+	queue_free()
+
