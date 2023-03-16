750472e2b9142470bd2d151a145bc0bf0a163897
stmSi <stm1998sithumyo@gmail.com>
2023-03-16 18:10:21 +0630

    Disable Lesson Button when no more lessons are available

--- src/main.gd
+++ src/main.gd
@@ -1,6 +1,14 @@
 extends Control
 
+@onready var take_lesson_btn: Button = %TakeLessonBtn
 
-func _on_test_game_btn_pressed() -> void:
-#	SceneChanger
-	pass
+func _ready() -> void:
+	var lesson_progress = UserProfileManager.load_lesson_progress()
+	var next_lesson = LessonAccess.get_next_lesson(
+		lesson_progress['lesson_number'],
+		lesson_progress['difficulty'],
+	)
+	
+	if next_lesson == []:
+		take_lesson_btn.disabled = true
+		take_lesson_btn.tooltip_text = "All Lessons Finished"
