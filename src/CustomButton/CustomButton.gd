51045e65a29da8514ed3bb459481ea7053ab5b8b
stmSi <stm1998sithumyo@gmail.com>
2023-03-16 18:11:33 +0630

    use CustomButton class to change pointing cursor shape

---
+++ src/CustomButton/CustomButton.gd
@@ -0,0 +1,5 @@
+extends Button
+class_name CustomButton
+
+func _ready() -> void:
+	mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
---
+++ src/CustomButton/CustomCheckButton.gd
@@ -0,0 +1,5 @@
+extends CheckButton
+class_name CustomCheckButton
+
+func _ready() -> void:
+	mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
