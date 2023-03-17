55d990a51db976b9e1ba8f927984be521194664a
stmSi <stm1998sithumyo@gmail.com>
2023-03-17 12:16:40 +0630

    Added: Settings to be able to disable Highlighting Current Character

--- src/Autoload/GeneralSettings.gd
+++ src/Autoload/GeneralSettings.gd
@@ -8,6 +8,7 @@ var general_settings_path = "user://general_settings.cfg"
 var config : ConfigFile = null
 
 signal popup_helper_disabled_changed(disabled: bool)
+signal hightlight_current_char_disabled_changed(disabled: bool)
 
 func _ready() -> void:
 	# Settings Config Setup
@@ -32,3 +33,20 @@ func set_popup_helper_disabled(disabled: bool) -> void:
 	config.set_value("Settings", "PopupHelperDisabled", disabled)
 	config.save(general_settings_path)
 	self.popup_helper_disabled_changed.emit(disabled)
+
+func get_hightlight_current_character_disabled() -> bool:
+	var err = config.load(general_settings_path)
+	if err != OK:
+		config.save(general_settings_path)
+		
+	return config.get_value("Settings", "HightlightCurrentCharacterDisabled", false)
+
+func set_hightlight_current_character_char(disabled: bool) -> void:
+	var err = config.load(general_settings_path)
+	if err != OK:
+		config = ConfigFile.new()
+
+	config.set_value("Settings", "HightlightCurrentCharacterDisabled", disabled)
+	config.save(general_settings_path)
+	self.hightlight_current_char_disabled_changed.emit(disabled)
+
