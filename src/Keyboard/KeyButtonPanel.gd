c1b80753c3b01982a4c36c16a2187b5ff28fba24
stmSi <stm1998sithumyo@gmail.com>
2023-03-25 16:45:34 +0630

    Added: Hover KeyButton in statistics page

---
+++ src/Keyboard/KeyButtonPanel.gd
@@ -0,0 +1,15 @@
+extends Panel
+
+@onready var key_button: Control = $".."
+#
+#func on_mouse_entered() -> void:
+#	EventBus.keybutton_hovered.emit(key_button)
+#	print('emitting from panel ... hovered')
+#	pass # Replace with function body.
+#
+#
+#func on_mouse_exited() -> void:
+#	EventBus.keybutton_unhovered.emit(key_button)
+#	print('emitting from panel ... unhovered')	
+#	pass # Replace with function body.
+#
