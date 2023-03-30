1f8e50238d647c278b587a32f17c6fb7d066adc7
stmSi <stm1998sithumyo@gmail.com>
2023-03-30 16:52:43 +0630

    More Exercises and Typo

--- src/ReportStatistics/report_statistics.gd
+++ src/ReportStatistics/report_statistics.gd
@@ -4,10 +4,10 @@ extends PanelContainer
 signal close
 
 func _ready() -> void:
-#	get_tree().paused = true
+	get_tree().paused = true
 	continue_btn.grab_focus()
 
 func _on_continue_btn_pressed() -> void:
 	queue_free()
+	get_tree().paused = false
 	self.close.emit()
-#	get_tree().paused = false
