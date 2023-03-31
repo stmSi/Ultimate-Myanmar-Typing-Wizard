f4c414c9094218bfe75c748d04dfb0df2fb613cf
stmSi <stm1998sithumyo@gmail.com>
2023-03-31 10:10:53 +0630

    FPS Settings

--- src/SettingsMenu/FPSOptions.gd
+++ src/SettingsMenu/FPSOptions.gd
@@ -1,5 +1,17 @@
 extends OptionButton
 
+func _ready() -> void:
+	_on_max_fps_changed(DisplaySettings.get_max_fps())
+	
+	DisplaySettings.max_fps_changed.connect(self._on_max_fps_changed)
 
 func _on_item_selected(index: int) -> void:
-	DisplaySettings.change_max_fps(int(get_item_text(index)))
+	var txt = get_item_text(index)
+	DisplaySettings.change_max_fps(int(txt))
+
+
+func _on_max_fps_changed(fps: int):
+	for i in self.item_count:
+		if fps == int(self.get_item_text(i)):
+			self.selected = i
+	
